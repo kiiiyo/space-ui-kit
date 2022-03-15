e48e9f99c8d9e414538fd107722801df5f3fa9f5
kiiiyo <kiiiyo.jp@gmail.com>
2022-03-15 14:16:20 +0900

    :sparkles: modify sign in form

--- src/components/organisms/SignInForm/SignInForm.tsx
+++ src/components/organisms/SignInForm/SignInForm.tsx
@@ -1,152 +1,130 @@
 import { FC } from 'react'
-import { css, Theme } from '@emotion/react'
-import { blue } from '@mui/material/colors'
+import { FormProvider, Controller } from 'react-hook-form'
 //
 import { Atoms } from '@/components'
-
-// Interface
+//
+import { useSignInForm } from './hooks'
+import type { TUseSignInFormProps } from './hooks'
 
 //  Style
+import {
+  containerStyle,
+  innerStyle,
+  headerStyle,
+  titleStyle,
+  alertStyle,
+  textFieldStyle,
+  formConfigStyle,
+  forgotPasswordLinkStyle
+} from './style'
 
-const containerStyle = (theme: Theme) =>
-  css({
-    padding: theme.spacing(4),
-    [theme.breakpoints.down('sm')]: {
-      padding: theme.spacing(2),
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1)
-    }
-  })
-
-const innerStyle = (theme: Theme) =>
-  css({
-    width: 460,
-    [theme.breakpoints.down('md')]: {
-      width: 512
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: '100%'
-    }
-  })
-
-const headerStyle = (theme: Theme) =>
-  css({
-    marginBottom: theme.spacing(5)
-  })
-
-const titleStyle = (theme: Theme) =>
-  css({
-    fontSize: '1.5rem',
-    marginBottom: '1rem',
-    [theme.breakpoints.down('md')]: {
-      fontSize: '1rem'
-    }
-  })
-
-const alertStyle = (theme: Theme) =>
-  css({
-    marginBottom: theme.spacing(2)
-  })
-
-const textFieldStyle = () =>
-  css({
-    display: 'block',
-    width: '100%'
-  })
-
-const formConfigStyle = (theme: Theme) =>
-  css({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2)
-  })
-
-const forgotPasswordLinkStyle = () =>
-  css({
-    marginLeft: 'auto',
-    textDecoration: 'underline',
-    fontWeight: 400,
-    fontSize: '0.875rem',
-    lineHeight: 1.5,
-    display: 'block',
-    '&:link': {
-      color: blue['600']
-    },
-    '&:visited': {
-      color: blue['600']
-    },
-    '&:hover': {
-      color: blue['500']
-    },
-    '&:active': {
-      color: blue['800']
-    }
-  })
+// Interface
+export type SignInFormPresenterProps = TUseSignInFormProps
 
 // Presenter
-
-export const SignInFormPresenter: FC = () => {
+export const SignInFormPresenter: FC<SignInFormPresenterProps> = ({
+  actions: { useFormMethods, onSubmit }
+}) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useFormMethods
   return (
-    <Atoms.Paper css={containerStyle} elevation={0}>
-      <Atoms.Box component="form" noValidate autoComplete="off">
-        <Atoms.Box css={innerStyle}>
-          <Atoms.Box css={headerStyle}>
-            <Atoms.Typography css={titleStyle} component="h1">
-              Sign in to 🚀 Space UI Kit
-            </Atoms.Typography>
-            <Atoms.Typography component="p">
-              Enter your details below.
-            </Atoms.Typography>
-          </Atoms.Box>
-          <Atoms.Box>
-            <Atoms.Alert severity="info" css={alertStyle}>
-              ID : demo@spaceui.kit / Password : demo1234
-            </Atoms.Alert>
-            <Atoms.Box css={textFieldStyle}>
-              <Atoms.TextField
-                label="ID"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-              />
-            </Atoms.Box>
-            <Atoms.Box css={textFieldStyle}>
-              <Atoms.TextField
-                label="Password"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-              />
+    <FormProvider {...useFormMethods}>
+      <Atoms.Paper css={containerStyle} elevation={0}>
+        <Atoms.Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Atoms.Box css={innerStyle}>
+            <Atoms.Box css={headerStyle}>
+              <Atoms.Typography css={titleStyle} component="h1">
+                Sign in to 🚀 Space UI Kit
+              </Atoms.Typography>
+              <Atoms.Typography component="p">
+                Enter your details below.
+              </Atoms.Typography>
             </Atoms.Box>
             <Atoms.Box>
-              <Atoms.Box css={formConfigStyle}>
-                <Atoms.FormControlLabel
-                  control={<Atoms.Checkbox defaultChecked />}
-                  label="Remember me"
+              <Atoms.Alert severity="info" css={alertStyle}>
+                ID : demo@spaceui.kit / Password : demo1234
+              </Atoms.Alert>
+              <Atoms.Box css={textFieldStyle}>
+                <Controller
+                  name="id"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Atoms.TextField
+                      label="ID"
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      error={errors.id ? true : false}
+                      helperText={errors.id ? errors.id?.message : ''}
+                      {...field}
+                    />
+                  )}
                 />
-                <Atoms.Typography
-                  component="a"
-                  href="#"
-                  css={forgotPasswordLinkStyle}
-                >
-                  Forgot password?
-                </Atoms.Typography>
               </Atoms.Box>
+              <Atoms.Box css={textFieldStyle}>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Atoms.TextField
+                      label="Password"
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      error={errors.password ? true : false}
+                      helperText={
+                        errors.password ? errors.password?.message : ''
+                      }
+                      {...field}
+                    />
+                  )}
+                />
+              </Atoms.Box>
+              <Atoms.Box>
+                <Atoms.Box css={formConfigStyle}>
+                  <Atoms.FormControlLabel
+                    control={<Atoms.Checkbox defaultChecked />}
+                    label="Remember me"
+                  />
+                  <Atoms.Typography
+                    component="a"
+                    href="#"
+                    css={forgotPasswordLinkStyle}
+                  >
+                    Forgot password?
+                  </Atoms.Typography>
+                </Atoms.Box>
 
-              <Atoms.Button size="large" variant="contained" fullWidth>
-                Login
-              </Atoms.Button>
+                <Atoms.Button
+                  type="submit"
+                  size="large"
+                  variant="contained"
+                  fullWidth
+                >
+                  Login
+                </Atoms.Button>
+              </Atoms.Box>
             </Atoms.Box>
           </Atoms.Box>
         </Atoms.Box>
-      </Atoms.Box>
-    </Atoms.Paper>
+      </Atoms.Paper>
+    </FormProvider>
   )
 }
 
 // Container
 export const SignInForm: FC = () => {
-  return <SignInFormPresenter />
+  const { actions } = useSignInForm()
+
+  return <SignInFormPresenter actions={actions} />
 }
