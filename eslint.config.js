c61c85ff44b7de3709f58bdce5e89bf565c57ee2
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-03 20:31:26 +0900

    chore: modify eslint config files

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+import typescriptEslintParser from '@typescript-eslint/parser';
+
+/* FIXME: Move eslint-configs directory */
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+
+/* FIXME: Move eslint-configs directory */
+import esLintPluginReact from 'eslint-plugin-react';
+import esLintPluginReactRefresh from 'eslint-plugin-react-refresh';
+import esLintPluginReactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  {
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+        ...globals.node,
+      },
+      parser: typescriptEslintParser,
+      parserOptions: {
+        ecmaVersion: 2023,
+        project: ['./**/tsconfig.json'],
+      },
+    },
+  },
+
+  /* Ignores */
+  {
+    ignores: ['dist', 'eslint.config.js', 'eslint-configs/*'],
+  },
+
+  js.configs.recommended,
+
+  /* Typescript */
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+  },
+
+  /* React */
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      react: esLintPluginReact,
+      'react-refresh': esLintPluginReactRefresh,
+      'react-hooks': esLintPluginReactHooks,
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+];
