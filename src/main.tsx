3c04ac9a4f617ac1c24cafbe4f1455ba20c4345e
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-11 13:57:03 +0900

    chore: fix lint format

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
-import App from './App.tsx';
+import App from './App';
 import './globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
