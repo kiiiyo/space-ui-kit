e0a93fb21ec6e62574ab81a1ff2c8a35205bfcdd
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-13 13:49:59 +0900

    feat: edit theme setting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
+import { ThemeProvider } from '@/providers';
+
 import App from './App';
 import './globals.css';
 
@@ -11,9 +13,11 @@ const root = createRoot(document.getElementById('app') as HTMLElement);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider defaultTheme="system" storageKey="space-ui-kit-theme">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </HelmetProvider>
   </React.StrictMode>,
 );
