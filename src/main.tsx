dd1dd3d8a2742c54ccb8a49840d069715a476751
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-18 14:46:53 +0900

    feat: add app provider component

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
-import { ThemeProvider } from '@/providers';
+import { ThemeProvider, AppProvider } from '@/providers';
 
 import App from './App';
 import './globals.css';
@@ -13,11 +13,13 @@ const root = createRoot(document.getElementById('app') as HTMLElement);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <ThemeProvider defaultTheme="system" storageKey="space-ui-kit-theme">
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <AppProvider>
+        <ThemeProvider defaultTheme="system" storageKey="space-ui-kit-theme">
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </AppProvider>
     </HelmetProvider>
   </React.StrictMode>,
 );
