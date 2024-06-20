ceb275f76845ca6512ed27a11da44320427db700
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-20 10:57:54 +0900

    feat: add presenter for FloatingNavigationBar component

---
+++ src/components/ui/floating-navigation-bar/floating-navigation-bar.presenter.ts
@@ -0,0 +1,27 @@
+import { useCallback } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import { useTheme } from '@/providers/theme-provider';
+
+import type { ThemeKind } from '@/providers/theme-provider';
+
+export function useFloatingNavigationBarPresenter() {
+  const navigate = useNavigate();
+  const { onSetTheme } = useTheme();
+
+  const onSelectThemeStyle = useCallback(
+    (theme: ThemeKind) => {
+      onSetTheme(theme);
+    },
+    [onSetTheme],
+  );
+
+  const onClickSignIn = useCallback(() => {
+    navigate('/sign-in');
+  }, [navigate]);
+
+  return {
+    onSelectThemeStyle,
+    onClickSignIn,
+  } as const;
+}
