8badc4ac856ac0148f4f2a575c7c23a89a51d9e9
kiiiyo <kiiiyo.jp@gmail.com>
2024-06-28 16:00:38 +0900

    feat: update WYSIWYGEditor component to use defaultContent prop

--- src/components/page/dashboard-issues-edit-page/dashboard-issues-edit-page.mock.tsx
+++ src/components/page/dashboard-issues-edit-page/dashboard-issues-edit-page.mock.tsx
@@ -1,27 +1,27 @@
+import { generateJSON } from '@tiptap/html';
+import StarterKit from '@tiptap/starter-kit';
 import { type FormSchemaType } from './dashboard-issues-edit-page.component';
+
+const mockDescription = `
+<h3>Subject of the issue</h3>
+<p>Describe your issue here.</p>
+<h3>Your environment</h3>
+<ul>
+<li>version of space-ui-kit</li>
+<li>version of react</li>
+<li>which browser and its version</li>
+</ul>
+<h3>Steps to reproduce</h3>
+<p>Tell us how to reproduce this issue. Please provide a working demo.</p>
+<h3>Expected behaviour</h3>
+<p>Tell us what should happen</p>
+<h3>Actual behaviour</h3>
+<p>Tell us what happens instead</p>`;
 
 export function generateMockEditIssue(): FormSchemaType {
   return {
     title: 'Issue Template',
-    description: `
-### Subject of the issue
-
-Describe your issue here.
-
-### Your environment
-* version of space-ui-kit
-* version of react
-* which browser and its version
-
-### Steps to reproduce
-Tell us how to reproduce this issue. Please provide a working demo.
-
-### Expected behaviour
-Tell us what should happen
-
-### Actual behaviour
-Tell us what happens instead
-    `,
+    description: generateJSON(mockDescription, [StarterKit]),
     singleLabel: 'singleLabelA',
     multipleLabels: ['multipleLabelB', 'multipleLabelC'],
   };
